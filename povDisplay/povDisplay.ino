e1cbb9c724d120c0860b4d6af345a625e18cc94b
hajiler <elijahrrbeltran@gmail.com>
2020-11-11 14:40:57 -0600

    Replaces map datastructure with 3D array using index system, reduces memory usage, adds header file for constants, compiles correctly

--- povDisplay/povDisplay.ino
+++ povDisplay/povDisplay.ino
@@ -1,99 +1,90 @@
-#include <ArduinoSTL.h>
-#include <map>
-#include <vector>
+#include "Constants.h"
 
-std::map<char, std::vector<int> > charToLED;
-// charToLED.emplace('9' , {1,1,1,1,0,0,0,1, 1,0,0,1,0,0,0,1, 1,0,0,1,0,0,0,1, 1,0,0,1,0,0,0,1, 1,1,1,1,1,1,1,1});
-// charToLED.emplace('8' , std::vector<int> ({0,1,1,0,1,1,1,0, 1,0,0,1,0,0,0,1, 1,0,0,1,0,0,0,1, 1,0,0,1,0,0,0,1, 0,1,1,0,1,1,1,0}));
-// charToLED.emplace('7' , std::vector<int> ({1,0,0,0,0,0,0,0, 1,0,0,0,1,0,0,0, 1,0,0,0,1,0,0,0, 1,0,0,1,1,1,1,1, 1,1,1,0,1,0,0,0}));
-// charToLED.emplace('5' , std::vector<int> ({1,1,1,1,1,0,0,1, 1,0,0,0,1,0,0,1, 1,0,0,0,1,0,0,1, 1,0,0,0,1,0,0,1, 1,0,0,0,1,1,1,1}));
-// charToLED.emplace('6' , std::vector<int> ({1,1,1,1,1,1,1,1, 1,0,0,0,1,0,0,1, 1,0,0,0,1,0,0,1, 1,0,0,0,1,0,0,1, 1,0,0,0,1,1,1,1}));
-// charToLED.emplace('4' , std::vector<int> ({1,0,0,0,1,0,0,0, 1,1,1,1,1,1,1,1, 1,0,0,0,1,0,0,0, 0,1,0,1,0,0,0,0, 0,0,1,0,0,0,0,0}));
-// charToLED.emplace('3' , std::vector<int> ({1,1,0,1,0,0,1,1, 1,0,0,1,0,0,0,1, 1,1,1,1,1,1,1,1, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0}));
-// charToLED.emplace('2' , std::vector<int> ({1,0,0,0,0,0,1,1, 1,0,0,0,0,1,0,1, 1,0,0,0,1,0,0,1, 1,0,0,1,0,0,0,1, 0,1,1,0,0,0,0,1}));
-// charToLED.emplace('1' , std::vector<int> ({0,0,1,0,0,0,0,0, 0,1,0,0,0,0,0,0, 1,1,1,1,1,1,1,1, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0}));
-// charToLED.emplace('0' , std::vector<int> ({1,1,1,1,1,1,1,1, 1,0,0,0,0,0,0,1, 1,0,0,0,0,0,0,1, 1,0,0,0,0,0,0,1, 1,1,1,1,1,1,1,1}));
-// charToLED.emplace('_' , std::vector<int> ({0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0}));
-// charToLED.emplace('A' , std::vector<int> ({1,1,1,1,1,1,1,1, 1,0,0,1,0,0,0,0, 1,0,0,1,0,0,0,0, 1,0,0,1,0,0,0,0, 1,1,1,1,1,1,1,1}));
-// charToLED.emplace('B' , std::vector<int> ({1,1,1,1,1,1,1,1, 1,0,0,1,0,0,0,1, 1,0,0,1,0,0,0,1, 1,0,0,1,0,0,0,1, 0,1,1,0,1,1,1,0}));
-// charToLED.emplace('C' , std::vector<int> ({0,0,1,1,1,1,0,0, 0,1,0,0,0,0,1,0, 1,0,0,0,0,0,0,1, 1,0,0,0,0,0,0,1, 1,0,0,0,0,0,0,1}));
-// charToLED.emplace('D' , std::vector<int> ({1,1,1,1,1,1,1,1, 1,0,0,0,0,0,0,1, 1,0,0,0,0,0,0,1, 0,1,0,0,0,0,1,0, 0,0,1,1,1,1,0,0}));
-// charToLED.emplace('E' , std::vector<int> ({1,1,1,1,1,1,1,1, 1,0,0,1,0,0,0,1, 1,0,0,1,0,0,0,1, 1,0,0,1,0,0,0,1, 1,0,0,1,0,0,0,1}));
-// charToLED.emplace('F' , std::vector<int> ({1,1,1,1,1,1,1,1, 1,0,0,1,0,0,0,0, 1,0,0,1,0,0,0,0, 1,0,0,1,0,0,0,0, 1,0,0,1,0,0,0,0}));
-// charToLED.emplace('G' , std::vector<int> ({0,1,1,1,1,1,1,1, 1,0,0,0,0,0,0,1, 1,0,0,0,1,0,0,1, 1,0,0,0,1,0,0,1, 1,0,0,0,1,1,1,0}));
-// charToLED.emplace('H' , std::vector<int> ({1,1,1,1,1,1,1,1, 0,0,0,0,1,0,0,0, 0,0,0,0,1,0,0,0, 0,0,0,0,1,0,0,0, 1,1,1,1,1,1,1,1}));
-// charToLED.emplace('I' , std::vector<int> ({1,0,0,0,0,0,0,1, 1,0,0,0,0,0,0,1, 1,1,1,1,1,1,1,1, 1,0,0,0,0,0,0,1, 1,0,0,0,0,0,0,1}));
-// charToLED.emplace('J' , std::vector<int> ({0,0,0,0,0,1,1,0, 0,0,0,0,1,0,0,1, 0,0,0,0,0,0,0,1, 0,0,0,0,0,0,0,1, 1,1,1,1,1,1,1,0}));
-// charToLED.emplace('K' , std::vector<int> ({1,1,1,1,1,1,1,1, 0,0,0,1,1,0,0,0, 0,0,1,0,0,1,0,0, 0,1,0,0,0,0,1,0, 1,0,0,0,0,0,0,1}));
-// charToLED.emplace('L' , std::vector<int> ({1,1,1,1,1,1,1,1, 0,0,0,0,0,0,0,1, 0,0,0,0,0,0,0,1, 0,0,0,0,0,0,0,1, 0,0,0,0,0,0,0,1}));
-// charToLED.emplace('M' , std::vector<int> ({1,1,1,1,1,1,1,1, 0,1,0,0,0,0,0,0, 0,0,1,0,0,0,0,0, 0,1,0,0,0,0,0,0, 1,1,1,1,1,1,1,1}));
-// charToLED.emplace('N' , std::vector<int> ({1,1,1,1,1,1,1,1, 0,0,1,0,0,0,0,0, 0,0,0,1,1,0,0,0, 0,0,0,0,0,1,0,0, 1,1,1,1,1,1,1,1}));
-// charToLED.emplace('O' , std::vector<int> ({0,1,1,1,1,1,1,0, 1,0,0,0,0,0,0,1, 1,0,0,0,0,0,0,1, 1,0,0,0,0,0,0,1, 0,1,1,1,1,1,1,0}));
-// charToLED.emplace('P' , std::vector<int> ({1,1,1,1,1,1,1,1, 1,0,0,1,0,0,0,0, 1,0,0,1,0,0,0,0, 1,0,0,1,0,0,0,0, 0,1,1,0,0,0,0,0}));
-// charToLED.emplace('Q' , std::vector<int> ({0,1,1,1,1,1,1,0, 1,0,0,0,0,0,0,1, 1,0,0,0,0,1,0,1, 0,1,1,1,1,1,1,0, 0,0,0,0,0,0,0,1}));
-// charToLED.emplace('R' , std::vector<int> ({1,1,1,1,1,1,1,1, 1,0,0,1,1,0,0,0, 1,0,0,1,0,1,0,0, 1,0,0,1,0,0,1,0, 0,1,1,0,0,0,0,1}));
-// charToLED.emplace('S' , std::vector<int> ({0,1,1,1,0,0,0,1, 1,0,0,0,1,0,0,1, 1,0,0,0,1,0,0,1, 1,0,0,0,1,0,0,1, 1,0,0,0,1,1,1,0}));
-// charToLED.emplace('T' , std::vector<int> ({1,0,0,0,0,0,0,0, 1,0,0,0,0,0,0,0, 1,1,1,1,1,1,1,1, 1,0,0,0,0,0,0,0, 1,0,0,0,0,0,0,0}));
-// charToLED.emplace('U' , std::vector<int> ({1,1,1,1,1,1,1,0, 0,0,0,0,0,0,0,1, 0,0,0,0,0,0,0,1, 0,0,0,0,0,0,0,1, 1,1,1,1,1,1,1,0}));
-// charToLED.emplace('V' , std::vector<int> ({1,1,1,1,1,1,0,0, 0,0,0,0,0,0,1,0, 0,0,0,0,0,0,0,1, 0,0,0,0,0,0,1,0, 1,1,1,1,1,1,0,0}));
-// charToLED.emplace('W' , std::vector<int> ({1,1,1,1,1,1,1,1, 0,0,0,0,0,0,1,0, 0,0,0,0,0,1,0,0, 0,0,0,0,0,0,1,0, 1,1,1,1,1,1,1,1}));
-// charToLED.emplace('X' , std::vector<int> ({1,1,0,0,0,0,1,1, 0,0,1,0,0,1,0,0, 0,0,0,1,1,0,0,0, 0,0,1,0,0,1,0,0, 1,1,0,0,0,0,1,1}));
-// charToLED.emplace('Y' , std::vector<int> ({1,1,0,0,0,0,0,0, 0,0,1,0,0,0,0,0, 0,0,0,1,1,1,1,1, 0,0,1,0,0,0,0,0, 1,1,0,0,0,0,0,0}));
+char message[10];
 
-
-
- // defining the space between the letters (ms)
- const int charSpacing = 4;
- // defining the time dots appear (ms) 
- const int delayTime =1; 
- const int pinOffSet = 3;
- 
-void setup() { 
-  std::map<int, char> x;
-  x.emplace(5, 'a');
-  Serial.begin(9600); 
-//  charToLED.emplace('Z' , std::vector<int>({1,0,0,0,0,1,1,1, 1,0,0,0,1,0,0,1, 1,0,0,1,0,0,0,1, 1,0,1,0,0,0,0,1, 1,1,0,0,0,0,0,1}));
-         
-  //Set 8 LED pins
-  for( int i = 2; i<10 ;i++ ) {
-    pinMode(i, OUTPUT);
-  }    
+const short getLEDConfigIndexFrom(char c) {
+  switch(c) {
+    case ('9') : return 0;
+    case ('8') : return 1;
+    case ('7') : return 2;
+    case ('5') : return 3;
+    case ('6') : return 4;
+    case ('4') : return 5;
+    case ('3') : return 6;
+    case ('2') : return 7;
+    case ('1') : return 8;
+    case ('0') : return 9;
+    case ('_') : return 10;
+    case ('A') : return 11;
+    case ('B') : return 12;
+    case ('C') : return 13;
+    case ('D') : return 14;
+    case ('E') : return 15;
+    case ('F') : return 16;
+    case ('G') : return 17;
+    case ('H') : return 18;
+    case ('I') : return 19;
+    case ('J') : return 20;
+    case ('K') : return 21;
+    case ('L') : return 22;
+    case ('M') : return 23;
+    case ('N') : return 24;
+    case ('O') : return 25;
+    case ('P') : return 26;
+    case ('Q') : return 27;
+    case ('R') : return 28;
+    case ('S') : return 29;
+    case ('T') : return 30;
+    case ('U') : return 31;
+    case ('V') : return 32;
+    case ('W') : return 33;
+    case ('X') : return 34;
+    case ('Y') : return 35;
+    case ('Z') : return 36;
+  }
 }
 
 //checks for serial input, reads and prints message to POV
 void getIncoming() {
-  char inputBuffer[64];
   if (Serial.available() > 0) {
-    Serial.readBytesUntil('\n', inputBuffer, 64);
+    Serial.readBytesUntil('\n', message, 10);
   }
   
   //print message
   int messageIndex = 0;
-  while (inputBuffer[messageIndex] != '#') {
-    printLetterToPOV(inputBuffer[messageIndex]);
+  while (message[messageIndex] != '#') {
+    printLetterToPOV(message[messageIndex]);
     messageIndex++;
   }
 }
 
-//finds led configuration for char, calls helper to print each part of design and a space for incoming character
+//Print single character to POV display
 void printLetterToPOV(char symbol) {
-  std::vector<int> charDesign = charToLED.find(symbol)->second;
+  //get the index for the current characters LED configuragtion
+  const short symbolConfigIndex = getLEDConfigIndexFrom(symbol);
   //print LED configuration as given by the above map
-  for (int charOffSet = 0; charOffSet < 24; charOffSet += 8) {
-    printColumnToLED(charOffSet, charDesign);
+  for (short curConfigColumn = 0; curConfigColumn < 5; curConfigColumn++) {
+    for (short curPin = LED_START; curPin <= LED_END; curPin++) {
+      int pinValue = (SYMBOL_CONFIGURATIONS[symbolConfigIndex][curConfigColumn][curPin - LED_START]) ? HIGH : LOW;
+      digitalWrite(curPin, pinValue);
+    }
   }
+  
   //print line spacing
-  for (int cur = 0; cur < 8; cur ++) {
-    digitalWrite(cur + pinOffSet, 0);
+  for (short curPin = LED_START; curPin <= LED_END; curPin++) {
+    digitalWrite(curPin, LOW);
   }
 }
 
-//helper to print led configuration to LEDs for one part of char
-void printColumnToLED(int charOffSet, std::vector<int> charDesign) {
-  for (int cur = 0; cur < 8; cur ++) {
-    digitalWrite(cur + pinOffSet, charDesign.at(cur + charOffSet));
-  }
-  delay(delayTime);
+
+ 
+void setup() { 
+  Serial.begin(9600); 
+ 
+  //Set 8 LED pins
+  for( int i = 2; i<10 ;i++ ) {
+    pinMode(i, OUTPUT);
+  }    
 }
  
 void loop() {
